227250e2d7c61eaecafd3775de889bae0850c3f9
Brian Bonsignore <bonsignore.brian@gmail.com>
2020-02-17 13:26:51 -0500

    Works, but could be cleaner

--- word_frequency.py
+++ word_frequency.py
@@ -4,10 +4,34 @@ STOP_WORDS = [
     'will', 'with'
 ]
 
+def clean_string(string):
+    """Remove whitespace and punctuation and converts to lowercase"""
+    s = ''
+    for char in string.lower():
+        if char.isalpha():
+            s+= char
+    return s
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
-    pass
+    collector = {}
+    just_width = 0
+    with open(file, 'r') as text:
+        lines = text.readlines()
+        for line in lines:
+            for word in line.split(' '):
+                word = clean_string(word)
+                if word not in STOP_WORDS and word in collector:
+                    collector[word] += '*'
+                    if len(word) > just_width:
+                        just_width = len(word)
+                elif word not in STOP_WORDS and word not in collector:
+                    collector[word] = '*'
+                    if len(word) > just_width:
+                        just_width = len(word)
+    for i,j in collector.items():
+        print(f'{i.rjust(just_width)} | {len(j)} {j}')
+    return collector
 
 
 if __name__ == "__main__":
